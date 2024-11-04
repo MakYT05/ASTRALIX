1666fa7cf4e2f80bcc75c1a7bc592076abf4a43d
Mak <makarpetrenko07@mail.ru>
2024-11-04 20:31:17 +0300

    mod

---
+++ src/main/java/org/zeith/modid/worldgen/biome/ModTerrablender.java
@@ -0,0 +1,9 @@
+//package org.zeith.modid.worldgen.biome;
+//
+//import net.minecraft.resources.ResourceLocation;
+//
+//public class ModTerrablender {
+//    public static void registerBiomes() {
+//        Regions.register(new ModOverworldRegion(new ResourceLocation("modid", "overworld"), 5));
+//    }
+//}
