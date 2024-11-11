287ed6ccffdfc61437aa71ab71f887e34ec61932
Mak <makarpetrenko07@mail.ru>
2024-11-11 22:31:52 +0300

    sword

--- src/main/java/org/zeith/modid/custom/items/AstralSword.java
+++ src/main/java/org/zeith/modid/custom/items/AstralSword.java
@@ -25,32 +25,33 @@ public class AstralSword extends Item {
     @Override
     public InteractionResultHolder<ItemStack> use(Level world, Player player, InteractionHand hand) {
         if (!world.isClientSide) {
-            if (ManaOverlay.currentMana >= 30) {
-                launchAstralSword(world, player, hand);
-            }
-            ManaOverlay.currentMana -= 30;
+            launchAstralSword(world, player, hand);
         }
         return InteractionResultHolder.success(player.getItemInHand(hand));
     }
 
     private void launchAstralSword(Level world, Player player, InteractionHand hand) {
-        Vec3 lookVec = player.getLookAngle();
-        Vec3 start = player.position().add(0, player.getEyeHeight(), 0);
-        Vec3 end = start.add(lookVec.scale(THROW_SPEED));
+        if (ManaOverlay.currentMana >= 30) {
+            Vec3 lookVec = player.getLookAngle();
+            Vec3 start = player.position().add(0, player.getEyeHeight(), 0);
+            Vec3 end = start.add(lookVec.scale(THROW_SPEED));
 
-        List<LivingEntity> entities = world.getEntitiesOfClass(LivingEntity.class, new AABB(start, end));
-        for (LivingEntity entity : entities) {
-            if (entity != player && !(entity instanceof FakePlayer)) {
-                entity.hurt(entity.damageSources().playerAttack(player), DAMAGE);
-                world.playSound(null, player.blockPosition(), SoundEvents.ANVIL_LAND, SoundSource.PLAYERS, 1.0F, 1.0F);
+            List<LivingEntity> entities = world.getEntitiesOfClass(LivingEntity.class, new AABB(start, end));
+            for (LivingEntity entity : entities) {
+                if (entity != player && !(entity instanceof FakePlayer)) {
+                    entity.hurt(entity.damageSources().playerAttack(player), DAMAGE);
+                    world.playSound(null, player.blockPosition(), SoundEvents.ANVIL_LAND, SoundSource.PLAYERS, 1.0F, 1.0F);
 
-                returnItemToPlayer(player, hand);
-                return;
+                    returnItemToPlayer(player, hand);
+                    return;
+                }
             }
-        }
 
-        world.playSound(null, player.blockPosition(), SoundEvents.ITEM_BREAK, SoundSource.PLAYERS, 1.0F, 1.0F);
-        returnItemToPlayer(player, hand);
+            world.playSound(null, player.blockPosition(), SoundEvents.ITEM_BREAK, SoundSource.PLAYERS, 1.0F, 1.0F);
+            returnItemToPlayer(player, hand);
+
+            ManaOverlay.currentMana -= 30;
+        }
     }
 
     private void returnItemToPlayer(Player player, InteractionHand hand) {
